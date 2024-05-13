bab8ae2a557438c681a7ff89a7e4e003a5427828
akatanate <rinmunakata@gmail.com>
2024-05-13 01:25:39 -0700

    fixed world. add details + submit

--- ASG3A/world/Cube.js
+++ ASG3A/world/Cube.js
@@ -4,6 +4,29 @@ class Cube {
       this.color = [1.0, 1.0, 1.0, 1.0];
       this.matrix = new Matrix4();
       this.textureNum = 0;
+      this.vertices = [
+        0,0,0, 1,1,0, 1,0,0,
+        0,0,0, 0,1,0, 1,1,0,
+        0,1,0, 0,1,1, 1,1,1,
+        0,1,0, 1,1,1, 1,1,0,
+        0,1,0, 0,1,1, 1,1,1,
+        0,1,0, 1,1,1, 1,1,0,
+        1,0,0, 1,1,1, 1,1,0,
+        1,0,0, 1,0,1, 1,1,1,
+        0,0,0, 0,1,1, 0,1,0,
+        0,0,0, 0,0,1, 0,1,1,
+        0,0,1, 1,1,1, 0,1,1,
+        0,0,1, 1,0,1, 1,1,1
+    ];
+    this.UVVertices  = [
+      0,0, 1,1, 1,0,  0,0, 0,1, 1,1,
+      0,0, 1,1, 1,0,  0,0, 0,1, 1,1,
+      0,0, 1,1, 1,0,  0,0, 0,1, 1,1,
+      0,0, 1,1, 1,0,  0,0, 0,1, 1,1,
+      0,0, 1,1, 1,0,  0,0, 0,1, 1,1,
+      0,0, 1,1, 1,0,  0,0, 0,1, 1,1
+  ];
+
     }
   
     render() {
@@ -105,50 +128,42 @@ class Cube {
 
       gl.uniformMatrix4fv(u_ModelMatrix, false, this.matrix.elements);
 
-      var allverts=[];
+      var allverts = [];
+      // Front 
+      allverts = allverts.concat([0.0,0.0,0.0, 1.0,1.0,0.0, 1.0,0.0,0.0 ]);
+      allverts = allverts.concat([0.0,0.0,0.0, 0.0,1.0,0.0, 1.0,1.0,0.0 ]);
+      // Back
+      allverts = allverts.concat([0.0,0.0,1.0, 1.0,1.0,1.0, 1.0,0.0,1.0 ]);
+      allverts = allverts.concat([0.0,0.0,1.0, 0.0,1.0,1.0, 1.0,1.0,1.0 ]);
+      // Top
+      allverts = allverts.concat([0.0,1.0,0.0, 1.0,1.0,0.0, 1.0,1.0,1.0 ]);
+      allverts = allverts.concat([0.0,1.0,1.0, 0.0,1.0,0.0, 1.0,1.0,1.0 ]);
+      // Bottom
+      allverts = allverts.concat([0.0,0.0,0.0, 0.0,0.0,1.0, 1.0,0.0,0.0 ]);
+      allverts = allverts.concat([1.0,0.0,0.0, 1.0,0.0,1.0, 0.0,0.0,1.0 ]);
+      // Left
+      allverts = allverts.concat([0.0,0.0,0.0, 0.0,1.0,0.0, 0.0,1.0,1.0 ]);
+      allverts = allverts.concat([0.0,1.0,1.0, 0.0,0.0,0.0, 0.0,0.0,1.0 ]);
+      // Right
+      allverts = allverts.concat([1.0,0.0,0.0, 1.0,1.0,0.0, 1.0,1.0,1.0 ]);
+      allverts = allverts.concat([1.0,1.0,1.0, 1.0,0.0,0.0, 1.0,0.0,1.0 ]);
 
-      // front 
-      allverts=allverts.concat( [ 0, 0, 0,      1, 1, 0,     1, 0, 0 ] );
-      allverts=allverts.concat( [ 0, 0, 0,      0, 1, 0,     1, 1, 0 ] );
-      // drawTriangle3DUV( [ 0, 0, 0,      1, 1, 0,     1, 0, 0 ], [ 1, 0,   0, 1,   1, 1] );
-      // drawTriangle3DUV( [ 0, 0, 0,      0, 1, 0,     1, 1, 0 ], [ 0, 0,   0, 1,   1, 1] );
-  
-      gl.uniform4f(u_FragColor, rgba[0]*.9, rgba[1]*.9, rgba[2]*.9, rgba[3]);
-
-      //UPDATE REST OF UV COORD****************************
-        // top
-        allverts=allverts.concat( [ 0, 1, 0,    0, 1, 1,    1, 1, 1 ] );
-        allverts=allverts.concat( [ 0, 1, 0,    0, 1, 1,    1, 1, 0 ] );
-        //drawTriangle3DUV( [ 0, 1, 0,    0, 1,1,    1, 1, 1 ], [0,0,   0,1,    1, 1]);
-        //drawTriangle3D( [ 0, 1, 0,    1, 1, 1,    1, 1, 0 ] );
-
-        // bottom
-        allverts=allverts.concat( [ 0, 0, 0,    1, 0, 0,    1, 0, 1 ] );
-        allverts=allverts.concat( [ 0, 0, 0,    1, 0, 1,    0, 0, 1 ] );
-        // drawTriangle3D( [ 0, 0, 0,    1, 0, 0,    1, 0, 1 ] );
-        // drawTriangle3D( [ 0, 0, 0,    1, 0, 1,    0, 0, 1 ] );
-
-        // right
-        allverts=allverts.concat( [ 1, 0, 0,    1, 0, 1,    1, 1, 1 ] );
-        allverts=allverts.concat( [ 1, 0, 0,   1, 1, 1,    1, 1, 0 ] );
-        // drawTriangle3D( [ 1, 0, 0,    1, 0, 1,    1, 1, 1 ] );
-        // drawTriangle3D( [ 1, 0, 0,   1, 1, 1,    1, 1, 0 ] );
-
-        //left
-        allverts=allverts.concat( [0, 0, 0,    0, 1, 1,    0, 1, 0 ]  );
-        allverts=allverts.concat( [ 0, 0, 0,   0, 1, 0,    0, 0, 1]  );
-        // drawTriangle3D( [0, 0, 0,    0, 1, 1,    0, 1, 0 ] );
-        // drawTriangle3D( [ 0, 0, 0,   0, 1, 0,    0, 0, 1] );
-
-        //back
-        allverts=allverts.concat( [1, 1, 1,    0, 1, 1,    0, 0, 1]   );
-        allverts=allverts.concat( [ 1, 1, 1,   0, 0, 1,    1, 0, 1 ] );
-        // drawTriangle3D( [1, 1, 1,    0, 1, 1,    0, 0, 1] );
-        // drawTriangle3D( [ 1, 1, 1,   0, 0, 1,    1, 0, 1 ] );
-
-        drawTriangle3D(allverts);
+      drawTriangle3D(allverts);
+   
     }
-  
+    
+    renderfaster(){
+      var rgba = this.color;                                   
+      
+      // Pass the texture number
+      gl.uniform1i(u_whichTexture, this.textureNum);
+      // Pass the color of point to u_FragColor
+      gl.uniform4f(u_FragColor, rgba[0], rgba[1], rgba[2], rgba[3]);  
+      // Pass the matrix to u_ModelMatrix attribute 
+      gl.uniformMatrix4fv(u_ModelMatrix, false, this.matrix.elements);
+      
+      drawTriangle3DUV(this.verticess, this.UVVertices);
+    }
   
   
   }
--- ASG3A/world/Triangle.js
+++ ASG3A/world/Triangle.js
@@ -171,3 +171,41 @@ function drawTriangle3DUV(vertices, uv){
   //return n;
 }  
  
+function drawTriangle3DUV(vertices, uv) {
+  var n = vertices.length; // The number of vertices
+
+  // Create a buffer object for verts
+  var vertexBuffer = gl.createBuffer();
+  if (!vertexBuffer) {
+      console.log('Failed to create the buffer object');
+      return -1;
+  }
+  // Bind the buffer object to target
+  gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+  // Write date into the buffer object
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.DYNAMIC_DRAW);
+  // Assign the buffer object to a_Position variable
+  gl.vertexAttribPointer(a_Position, 3, gl.FLOAT, false, 0, 0);
+  // Enable the assignment to a_Position variable
+  gl.enableVertexAttribArray(a_Position);
+
+
+  // Create a buffer object for UV
+  var uvBuffer = gl.createBuffer();
+  if (!uvBuffer) {
+      console.log('Failed to create the buffer object');
+      return -1;
+  }
+  // Bind the buffer object to target
+  gl.bindBuffer(gl.ARRAY_BUFFER, uvBuffer);
+  // Write date into the buffer object
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(uv), gl.DYNAMIC_DRAW);
+  // Assign the buffer object to a_Position variable
+  gl.vertexAttribPointer(a_UV, 2, gl.FLOAT, false, 0, 0);
+  // Enable the assignment to a_Position variable
+  gl.enableVertexAttribArray(a_UV);
+  
+  gl.drawArrays(gl.TRIANGLES, 0, n/3);
+
+  g_vertexBuffer = null;
+} 
